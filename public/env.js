1ed6201cb834233cb4388a04442f928493c939ea
User Name <robwistrand@gmail.com>
2025-05-10 10:23:21 -0500

    fix: remove hardcoded secrets and API tokens

--- public/env.js
+++ public/env.js
@@ -1,4 +1,4 @@
 // This script injects environment variables into the browser
 window.env = {
-  REACT_APP_HF_API_TOKEN: 'YOUR_HUGGING_FACE_TOKEN_HERE' // Will be replaced at runtime with environment values
+  REACT_APP_HF_API_TOKEN: '' // Will be injected at runtime from secure environment variables
 }; 
