7571aa4208772b9cdc6ef126690007927e815189
Rob Wistrand <robwistrand@gmail.com>
2025-05-19 19:34:27 -0700

    Update database configuration for Heroku

--- server/config/database.js
+++ server/config/database.js
@@ -20,18 +20,14 @@ module.exports = {
     logging: false
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
-    logging: false,
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false
       }
-    }
+    },
+    logging: false
   }
 }; 
